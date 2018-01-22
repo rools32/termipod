17959ed73c0de32d3a94cc730bc2bf28e163b0c9
rools <rools32@users.noreply.github.com>
2018-01-22 01:53:27 +0100

    Add no UI mode

--- pypod.py
+++ pypod.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
 from itemlist import ItemList
-from ui import start as uiStart
+from ui import start
+import argparse
+import sys
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(\
+        description='Manage your podcasts\nNo argument for UI')
+parser.add_argument('--add', type=str,
+                    help='Add channel')
+parser.add_argument('--up', action='store_true',
+                    help='Update channels')
+args = parser.parse_args()
 
 itemList = ItemList('pypod.db')
 
-uiStart(itemList)
+if args.up:
+    itemList.updateVideos()
+if args.add:
+    itemList.addChannel(args.add)
+if len(sys.argv) == 1:
+    start(itemList)
