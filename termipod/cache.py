1524076d5f3ab005e012845ff08cca86000137ce
rools <rools32@users.noreply.github.com>
2020-06-21 01:11:54 +0200

    Add locks in cache

--- termipod/cache.py
+++ termipod/cache.py
@@ -17,9 +17,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import os.path
+from pathlib import Path
 import urllib
 from collections import OrderedDict
 import hashlib
+from threading import Lock
 
 import termipod.config as Config
 from termipod.backends import get_download_func, DownloadError
@@ -27,6 +29,8 @@ from termipod.backends import get_download_func, DownloadError
 # Ordered dict by date, contains file size as value
 files = {}
 total_file_size = {}
+
+cache_lock = Lock()
 
 
 def filename_get_path(what, filename=''):
@@ -50,53 +54,64 @@ def item_get_filename(item, what):
     return f'{h}{ext}'
 
 
-def item_get_cache(item, what, print_infos):
-    # Build file list sorted by age
-    if what not in files:
-        filenames = os.listdir(filename_get_path(what))
-        filelist = [(f, os.stat(filename_get_path(what, f)))
-                    for f in filenames]
-        filelist.sort(key=lambda f: f[1].st_ctime, reverse=True)
-        files[what] = OrderedDict((f[0], f[1].st_size) for f in filelist)
-        total_file_size[what] = sum(files[what].values())
+def item_get_cache(item, what, print_infos, check_only=False):
+    cache_lock.acquire()
+    item_locks = item.setdefault('cache_lock', {})
+    item_lock = item_locks.setdefault(what, Lock())
+    cache_lock.release()
 
-    if not item[what]:
-        return ''
+    with item_lock:
+        # Build file list sorted by age
+        if what not in files:
+            filenames = os.listdir(filename_get_path(what))
+            filelist = [(f, os.stat(filename_get_path(what, f)))
+                        for f in filenames]
+            filelist.sort(key=lambda f: f[1].st_ctime, reverse=True)
+            files[what] = OrderedDict((f[0], f[1].st_size) for f in filelist)
+            total_file_size[what] = sum(files[what].values())
 
-    filename = item_get_filename(item, what)
-    filepath = filename_get_path(what, filename)
+        if not item[what]:
+            return ''
 
-    url = item[what]
+        filename = item_get_filename(item, what)
+        filepath = filename_get_path(what, filename)
 
-    if not os.path.isfile(filepath):
-        if what == 'link':
-            dlfunc = get_download_func(item)
-            try:
-                dlfunc(url, filepath, print_infos)
-            except DownloadError:
-                print_infos('Cannot access to %s' % url, mode='error')
+        url = item[what]
+
+        if not os.path.isfile(filepath):
+            if check_only:
                 return ''
+            if what == 'link':
+                dlfunc = get_download_func(item)
+                try:
+                    dlfunc(url, filepath, print_infos)
+                except DownloadError:
+                    print_infos('Cannot access to %s' % url, mode='error')
+                    return ''
+
+            else:
+                try:
+                    urllib.request.urlretrieve(url, filepath)
+                except urllib.error.URLError:
+                    return ''
+
+            file_size = os.stat(filepath).st_size
+            files[what][filename] = file_size
+
+            total_file_size[what] += file_size
+            max_size = 1024**2*Config.get('Global.'+what+'_max_total_mb')
+            while total_file_size[what] > max_size and len(files[what]) > 1:
+                f, size = files[what].popitem(last=False)
+                try:
+                    os.unlink(filename_get_path(what, f))
+                    total_file_size[what] -= size
+                except OSError:
+                    pass
 
         else:
-            try:
-                urllib.request.urlretrieve(url, filepath)
-            except urllib.error.URLError:
-                return ''
+            files[what].move_to_end(filename)
 
-        file_size = os.stat(filepath).st_size
-        files[what][filename] = file_size
+        if not check_only:
+            Path(filepath).touch()
 
-        total_file_size[what] += file_size
-        max_size = 1024**2*Config.get('Global.'+what+'_max_total_mb')
-        while total_file_size[what] > max_size and len(files[what]) > 1:
-            f, size = files[what].popitem(last=False)
-            try:
-                os.unlink(filename_get_path(what, f))
-                total_file_size[what] -= size
-            except OSError:
-                pass
-
-    else:
-        files[what].move_to_end(filename)
-
-    return filepath
+        return filepath
